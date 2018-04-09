b060644876436753f21a1ad32dc1c7d3cb0a2f9a
单厚旺 <shanhouwang@zbj.com>
2018-04-09 16:52:37 +0800

    Okhttp的责任链模式

--- src/CallServerInterceptor.java
+++ src/CallServerInterceptor.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
 
+/**
+ * 这个是真正处理请求的类
+ */
 public class CallServerInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
         Util.println(CallServerInterceptor.class.getSimpleName());
-        return new Response();
+        return new Response(chain.request());
     }
 }
--- src/Response.java
+++ src/Response.java
@@ -1,2 +1,8 @@
 public class Response {
+
+    public Request request;
+
+    public Response(Request request) {
+        this.request = request;
+    }
 }
